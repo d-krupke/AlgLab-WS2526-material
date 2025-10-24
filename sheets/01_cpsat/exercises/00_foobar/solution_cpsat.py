ec80ab70588d0b72e5b12d8004c0cddf8fddf1ab
Valentin Sarezki <v.sarezki@tu-braunschweig.de>
2025-10-24 16:20:15 +0200

    cpsat solution 00

--- sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
@@ -8,6 +8,19 @@ def solve(instance: Instance) -> Solution:
     """
     numbers = instance.numbers
     model = cp_model.CpModel()
+
+    domain = cp_model.Domain.from_values(numbers)
+    max_num = model.new_int_var_from_domain(domain, "max_num")
+    min_num = model.new_int_var_from_domain(domain, "min_num")
+
+    model.maximize(max_num - min_num)
+
+    solver = cp_model.CpSolver()
+    solver.solve(model)
+
+    numbers[0] = solver.value(max_num)
+    numbers[-1] = solver.value(min_num)
+
     return Solution(
         number_a=numbers[0],
         number_b=numbers[-1],
