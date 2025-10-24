19e8868572245c786e19fa583f044536aee2dbd2
Valentin Sarezki <v.sarezki@tu-braunschweig.de>
2025-10-24 13:20:06 +0200

    pure python 00

--- sheets/01_cpsat/exercises/00_foobar/solution_python.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_python.py
@@ -6,6 +6,12 @@ def solve(instance: Instance) -> Solution:
     Implement your solver for the problem here!
     """
     numbers = instance.numbers
+
+    for i in range(len(numbers)):
+        for j in range(0, len(numbers)-i-1):
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+
     return Solution(
         number_a=numbers[0],
         number_b=numbers[-1],
